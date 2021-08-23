a6d642a8b3805bdb74bde2a2a6d785969c257de8
poychang <poypost@gmail.com>
2021-08-23 16:51:38 +0800

    init

---
+++ Custom-MagicCommand/magic-command-load-env-file-extension/LoadEnvFileExtension/LoadEnvFileKernelExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Formatting;
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LoadEnvFileExtension
+{
+    public class LoadEnvFileKernelExtension : IKernelExtension
+    {
+        public Task OnLoadAsync(Kernel kernel)
+        {
+            var loadEnvCommand = new Command("#!env", "Load .env to .NET Notebook.")
+            {
+                new Option<string>(new[]{ "-f", "--file-path" }, "The .env file path"),
+                new Option<string>(new[]{ "-n", "--var-name" }, "The variable name which contain the .env setting"),
+            };
+
+            loadEnvCommand.Handler = CommandHandler.Create(
+                async (string filePath, string varName, KernelInvocationContext invocationContext) =>
+                {
+                    invocationContext.Display($"Load {filePath}");
+
+                    var filePathString = string.IsNullOrEmpty(filePath) ? string.Empty : Path.Combine(Environment.CurrentDirectory, filePath);
+                    Console.WriteLine($".env file path: {filePathString}");
+                    var command = new SubmitCode($"var {varName} = dotenv.net.DotEnv.Fluent().WithEnvFiles(\"{filePathString.Replace(@"\", @"\\")}\").Read();");
+
+                    await invocationContext.HandlingKernel.SendAsync(command);
+                });
+
+            kernel.AddDirective(loadEnvCommand);
+
+            if (KernelInvocationContext.Current is { } context)
+            {
+                PocketView view = PocketViewTags.div(
+                    PocketViewTags.code(nameof(LoadEnvFileExtension)), " is loaded. It adds loading feature for .env file.", PocketViewTags.br,
+                    "Try it by running: ", PocketViewTags.code("#!env -f [file-path] -n [variable-name]")
+                );
+
+                context.Display(view);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Play with .NET Notebook
+
+玩轉 .NET Interactive Notebooks 一次就上手
+
+https://www.facebook.com/events/412481300211387
+
+從 Visual Studio Code 1.59 開始，微軟將 Jupyter Notebook (*.ipynb) 轉變成內建的擴充套件，可見這種類型的文件越來越受重視。微軟這幾年也投入 .NET Interactive Notebooks 的實作，目前已經稍有雛形，它除了讓你在筆記本裡面寫下詳細的說明文件，還可以和各種語言實作 Mix 在一起，然後還可以讓你直接在編輯器中反覆執行、驗證，相當便利！👍
+
+本次直播我將邀請到目前任職於 Kingston 的全端工程師，同時也是 Microsoft MVP 的 Poy Chang 來為我們深入剖析這套工具，一個 .NET 開發者一定要擁有的絕佳利器，各位走過、路過、千萬不能錯過啊！🔥 顯示較少內容
